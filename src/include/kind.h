c22669ba5d285be5ecb9d7398e0d8666e0cda470
Ciremun <Ciremun@yandex.ru>
2021-06-20 19:46:30 +0300

    sort kinds

--- src/include/kind.h
+++ src/include/kind.h
@@ -3,32 +3,36 @@
 
 enum Kind: int
 {
+    eof,
+    error,
     number,
     space,
+    open_paren,
+    close_paren,
+
     plus,
     minus,
     star,
     forward_slash,
-    open_paren,
-    close_paren,
-    error,
-    eof,
+
     number_expr,
     binary_expr,
     paren_expr
 };
 
 constexpr const char* kinds[] = {
+    "eof",
+    "error",
     "number",
     "space",
+    "open_paren",
+    "close_paren",
+
     "plus",
     "minus",
     "star",
     "forward_slash",
-    "open_paren",
-    "close_paren" ,
-    "error",
-    "eof",
+
     "number_expr",
     "binary_expr",
     "paren_expr"
